fcbe181534d0581b9530f543639949cf32572749
Alec Deason <alec50@tinycountry.com>
2018-11-11 12:57:27 -0800

    Structures that make more sense with Rust's requirements

--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,200 @@
 extern crate rand;
 
 mod names;
+
+use std::fmt;
 use rand::Rng;
+use std::rc::Rc;
 use rand::distributions::{Normal, Distribution};
+use std::cell::RefCell;
 
-pub struct Event {
-    agent: usize,
-    location: usize,
-    time: f64,
-    desc: String,
+const STEP_SIZE: f64 = 1.0/(360.0*24.0);
+
+pub struct World {
+    pub time: f64,
+    agents: Vec<Rc<RefCell<Agent>>>,
+    locations: Vec<Rc<Location>>,
 }
 
+impl World {
+    pub fn new() -> World {
+        let mut rng = rand::thread_rng();
 
-pub struct Location {
-    agents: Vec<usize>,
-    capacity: usize,
-    name: &'static str,
-    exits: Vec<usize>,
-}
+        let locations: Vec<Rc<Location>> = (0..10000).into_iter()
+            .map(|id| Rc::new(Location::new(id)))
+            .collect();
 
-impl Location {
-    pub fn new() -> Location {
-        Location { agents: Vec::new(), capacity: 10, name: &"The Park", exits: Vec::new(), }
+        let mut w = World {
+            time: 0.0,
+            agents: Vec::new(),
+            locations: locations,
+        };
+
+        let loc_count = w.locations.len();
+
+        for loc in &w.locations {
+            for _ in 0..rng.gen_range(1, 10) {
+                let idx = rng.gen_range(0, loc_count);
+                let exit = w.locations[idx].clone();
+                loc.exits.borrow_mut().push(exit);
+            }
+        }
+
+        for id in 0..1000 {
+            let idx = rng.gen_range(0, loc_count);
+            let loc = w.locations[idx].clone();
+            let a = Rc::new(RefCell::new(Agent::new(id, loc.clone())));
+            loc.agents.borrow_mut().push(a.clone());
+            w.agents.push(a.clone());
+        }
+
+        w
+    }
+
+    pub fn step_simulation(&mut self) {
+        for agent_rc in &self.agents {
+            let old_location;
+            let new_location;
+            {
+                let mut agent = agent_rc.borrow_mut();
+                if !agent.health.alive { continue; };
+                old_location = agent.location.clone();
+                agent.step_simulation();
+                new_location = agent.location.clone();
+            }
+
+            if old_location != new_location {
+                old_location.agents.borrow_mut().remove_item(agent_rc);
+                new_location.agents.borrow_mut().push(agent_rc.clone());
+            }
+        }
+        self.time += 1.0;
+    }
+
+    pub fn show_events(&self, agent_index: usize) {
+        let a = self.agents[agent_index].borrow();
+        println!("Events for: {}", a);
+        for event in &a.events {
+            println!("{}", event.msg);
+        }
     }
 }
 
-#[derive(Copy, Clone)]
+struct Event {
+    msg: String,
+}
+
+#[derive(Copy, Clone, PartialEq)]
 enum Sex {
     Male,
     Female,
+}
+
+impl fmt::Display for Sex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Sex::Male => write!(f, "Male"),
+            Sex::Female => write!(f, "Female"),
+        }
+    }
+}
+
+struct Agent {
+    id: usize,
+    name: String,
+    sex: Sex,
+    age: f64,
+    health: Health,
+    mind: Mind,
+    location: Rc<Location>,
+    events: Vec<Event>,
+
+    action_points: u32,
+}
+
+impl PartialEq for Agent {
+    fn eq(&self, other: &Agent) -> bool {
+        self.id == other.id
+    }
+}
+
+impl fmt::Display for Agent {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {}, {})", self.name, self.sex, self.age)
+    }
+}
+
+impl Agent {
+    fn new(id: usize, location: Rc<Location>) -> Agent {
+        let mut rng = rand::thread_rng();
+        let sex = *rng.choose(&[Sex::Male, Sex::Female]).unwrap();
+        //TODO Normal isn't the right distribution for this
+        let age = match sex {
+            Sex::Female => Normal::new(38.1, 10.0).sample(&mut rng).min(120.0).max(0.0),
+            Sex::Male => Normal::new(36.8, 10.0).sample(&mut rng).min(120.0).max(0.0),
+        };
+        let first_name = match sex {
+            Sex::Male => rng.choose(names::MALE_FIRST_NAMES).unwrap(),
+            Sex::Female => rng.choose(names::FEMALE_FIRST_NAMES).unwrap(),
+        };
+        let surname = rng.choose(names::SURNAMES).unwrap();
+
+        let full_name = format!("{} {}", first_name, surname).to_string();
+
+        let mut a = Agent {
+            id: id,
+            name: full_name,
+            sex: sex,
+            age: age,
+            health: Health::new(),
+            mind: Mind::new(),
+            location: location,
+            events: Vec::new(),
+            action_points: 0,
+        };
+
+        a.events.push(Event { msg: "Agent created".to_string() });
+
+        a
+    }
+
+    fn step_simulation(&mut self) {
+        let view = HealthView { mind: &self.mind, location: self.location.clone() };
+        self.health.step_simulation(view);
+
+        if !self.health.alive {
+            self.events.push(Event { msg: "Died".to_string() });
+            return;
+        }
+
+        let view = MindView { health: &self.health, location: self.location.clone() };
+        self.mind.step_simulation(view);
+
+        self.age += STEP_SIZE;
+
+        self.action_points += 1;
+
+        if self.action_points >= 5 {
+            self.action_points -= 5;
+            let mut rng = rand::thread_rng();
+            let loc = self.location.clone();
+            let exits = loc.exits.borrow();
+            let idx = rng.gen_range(0, exits.len());
+            let new_location = exits[idx].clone();
+            self.events.push(Event { msg: format!("Moved from {} to {}", self.location.id, new_location.id).to_string() });
+            self.location = new_location;
+        }
+    }
+}
+
+struct HealthView<'a> {
+    mind: &'a Mind,
+    location: Rc<Location>,
+}
+
+struct MindView<'a> {
+    health: &'a Health,
+    location: Rc<Location>,
 }
 
 struct Health {
@@ -38,6 +204,7 @@ struct Health {
     pain: f64,
     hunger: f64,
     alive: bool,
+
 }
 
 impl Health {
@@ -52,7 +219,7 @@ impl Health {
         }
     }
 
-    fn step(&mut self) {
+    fn step_simulation(&mut self, view: HealthView) {
         if self.hunger == 1.0 {
             self.acute -= 0.01;
         }
@@ -62,7 +229,7 @@ impl Health {
         } else {
             self.pain -= 0.1;
         }
-        
+
         if self.acute < 0.2 {
             self.chronic -= 0.01;
         }
@@ -73,131 +240,57 @@ impl Health {
 
         self.hunger += 0.01;
         self.acute += 0.001;
-    
+
         self.chronic = self.chronic.min(1.0).max(0.0);
         self.acute = self.acute.min(1.0).max(0.0);
         self.pain = self.pain.min(1.0).max(0.0);
         self.hunger = self.hunger.min(1.0).max(0.0);
+
     }
 }
 
-pub struct Agent {
-    pub name: String,
-    events: Vec<Event>,
-    age: f64,
-    sex: Sex,
-    location: usize,
-    health: Health,
-
-    action_points: i32,
+struct Mind {
+    encounters: Vec<u32>,
 }
 
-impl Agent {
-    pub fn new() -> Agent {
-        let mut rng = rand::thread_rng();
-        let sex = *rng.choose(&[Sex::Male, Sex::Female]).unwrap();
-        let age = rng.gen_range(0.0, 120.0);
-        let first_name = match sex {
-            Sex::Male => rng.choose(names::MALE_FIRST_NAMES).unwrap(),
-            Sex::Female => rng.choose(names::FEMALE_FIRST_NAMES).unwrap(),
-        };
-        let surname = rng.choose(names::SURNAMES).unwrap();
-
-        let full_name = format!("{} {}", first_name, surname).to_string();
-
-        Agent { events: Vec::new(), name: full_name, age: age, sex: sex, location: 0, health: Health::new(), action_points: 0, }
-    }
-}
-
-pub struct World {
-    pub agents: Vec<Agent>,
-    locations: Vec<Location>,
-    pub time: f64,
-
-    rng: rand::ThreadRng,
-}
-
-impl World {
-    pub fn new() -> World {
-        let mut w = World {
-            agents: Vec::new(),
-            locations: Vec::new(),
-            time: 0.0,
-
-            rng: rand::thread_rng(),
-        };
-        w.initialize_locations();
-        w.initialize_agents();
-        w
-    }
-
-    fn initialize_locations(&mut self) {
-        for _ in 0..1000 {
-            self.locations.push( Location::new() );
-        }
-
-        for _ in 0..5000 {
-            let a = self.rng.gen_range(0, self.locations.len());
-            let b = self.rng.gen_range(0, self.locations.len());
-            self.locations[a].exits.push(b);
+impl Mind {
+    fn new() -> Mind {
+        Mind {
+            encounters: Vec::new(),
         }
     }
 
-    fn initialize_agents(&mut self) {
-        for _ in 0..1000 {
-            let mut place: usize;
-            while {place = self.rng.gen_range(0, self.locations.len()); self.locations[place].capacity <= self.locations[place].agents.len()} {}
-            self.locations[place].agents.push(self.agents.len());
-            let mut a = Agent::new();
-            a.location = place;
-            a.events.push( Event { location: place, agent: self.agents.len(), time: self.time, desc: "Agent created".to_string() } );
-            self.agents.push( a );
-        }
-    }
-
-    pub fn show_events(&self, agent: usize) {
-        for event in &self.agents[agent].events {
-            println!("{}: {} {}", event.time, self.agents[event.agent].name, event.desc);
-        }
-    }
-
-    pub fn step_simulation(&mut self) {
-        let mut index: Vec<usize> = (0..self.agents.len()).collect();
-        self.rng.shuffle(&mut index);
-        for i in index {
-            let a = &mut self.agents[i];
-            if a.health.alive {
-                a.health.step();
-                if !a.health.alive {
-                    a.events.push( Event { location: a.location,
-                                           agent: i,
-                                           time: self.time,
-                                           desc: "Died".to_string(),
-                    } );
-                    break
-                }
-                a.age += 1.0;
-                a.action_points += 1;
-                if a.action_points > 5 {
-                    let new_location;
-                    {
-                        let place = &self.locations[a.location];
-                        if place.exits.is_empty() { continue; }
-                        let exit = self.rng.gen_range(0, place.exits.len());
-                        new_location = self.locations[a.location].exits[exit];
-                    }
-                    self.locations[a.location].agents.remove_item(&i);
-                    self.locations[new_location].agents.push(i);
-                    a.events.push( Event { location: a.location,
-                                           agent: i,
-                                           time: self.time,
-                                           desc: format!("Moved from {} to {}", a.location, new_location),
-                    } );
-                    a.location = new_location;
-                    a.action_points -= 5;
-                }
+    fn step_simulation(&mut self, view: MindView) {
+        for a in view.location.agents.borrow().iter() {
+            match a.try_borrow() {
+                Ok(a) => {
+                    while self.encounters.len() <= a.id { self.encounters.push(0) }
+                    self.encounters[a.id] += 1;
+                },
+                Err(_) => () // This is the current agent, fine.
             }
         }
-        self.time += 1.0;
+    }
+}
+
+struct Location {
+    id: usize,
+    exits: RefCell<Vec<Rc<Location>>>,
+    agents: RefCell<Vec<Rc<RefCell<Agent>>>>,
+}
+
+
+impl PartialEq for Location {
+    fn eq(&self, other: &Location) -> bool {
+        self.id == other.id
+    }
+}
+impl Location {
+    fn new(id: usize) -> Location {
+        Location {
+            id: id,
+            exits: RefCell::new(Vec::new()),
+            agents: RefCell::new(Vec::new()),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ extern crate novel_gen;
 
 fn main() {
     let mut w = novel_gen::World::new();
-    while w.time < 1000.0 {
+    while w.time < 100.0 {
         w.step_simulation();
+        eprintln!("{}", w.time);
     }
     w.show_events(0);
 }
