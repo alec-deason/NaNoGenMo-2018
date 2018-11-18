d5abb146edc08d81ee5f8573373ff2ccf2bccc1f
Alec Deason <alec50@tinycountry.com>
2018-11-17 21:36:04 -0800

    Memory for paths and learning about things

--- src/conversation.rs
+++ src/conversation.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
 use rand::prelude::SliceRandom;
 use rand::prelude::IteratorRandom;
 
-use super::{Agent, World};
+use super::{Agent};
 
 pub struct Conversation {
     pub tone: f64,
@@ -23,8 +24,9 @@ impl Conversation {
                   Topic::Relationship { id_a, id_b, tone } => {
                       "a relationship".to_string()
                   },
-                  Topic::Thing { id } => "Some thing".to_string(), //w.items[*id].name.clone(),
+                  Topic::Thing { o_id, p_id } => "Some thing".to_string(), //w.items[*id].name.clone(),
                   Topic::Place { id } => "a place".to_string(),
+                  Topic::Path { id_a, id_b } => "a path".to_string(),
                   Topic::SmallTalk => "nothing in particular".to_string(),
               };
               topics.push(topic_str);
@@ -38,8 +40,9 @@ impl Conversation {
 pub enum Topic {
     Person { id: usize },
     Relationship { id_a: usize, id_b: usize, tone: f64},
-    Thing { id: usize },
+    Thing { o_id: usize, p_id: usize },
     Place { id: usize },
+    Path { id_a: usize, id_b: usize },
     SmallTalk,
 }
 
@@ -48,9 +51,10 @@ pub fn simulate_conversation(a: &Agent, b: &Agent) -> Conversation {
     let a_b_tone = a.mind.opinions_on_others.get(b.id).unwrap_or(&0.0);
     let b_a_tone = b.mind.opinions_on_others.get(a.id).unwrap_or(&0.0);
 
-    let topic = match ["people", "place", "thing", "nothing"].choose(&mut rng).unwrap() {
+    let topic = match ["people", "place", "path", "thing", "nothing"].choose(&mut rng).unwrap() {
         &"people" => who_can_we_talk_about(a, b),
         &"place" => where_can_we_talk_about(a, b),
+        &"path" => what_path_can_we_talk_about(a, b),
         &"thing" => what_can_we_talk_about(a, b),
         _ => Topic::SmallTalk,
     };
@@ -85,9 +89,9 @@ fn who_can_we_talk_about(a: &Agent, b: &Agent) -> Topic {
 
 fn what_can_we_talk_about(a: &Agent, b: &Agent) -> Topic {
     let mut rng = rand::thread_rng();
-    let id = a.mind.objects_seen.keys().chain(b.mind.objects_seen.keys()).choose(&mut rng);
-    match id {
-        Some(id) => Topic::Thing { id: *id },
+    let thing = a.mind.objects_seen.iter().chain(b.mind.objects_seen.iter()).choose(&mut rng);
+    match thing {
+        Some((o_id, p_id)) => Topic::Thing { o_id: *o_id, p_id: *p_id },
         None => Topic::SmallTalk,
     }
 }
@@ -106,6 +110,29 @@ fn where_can_we_talk_about(a: &Agent, b: &Agent) -> Topic {
         let mut rng = rand::thread_rng();
         let ids:Vec<usize> = (0..len).collect();
         Topic::Place { id: *ids.choose_weighted(&mut rng, |id| subject_weights[*id]).unwrap() }
+    } else {
+        Topic::SmallTalk
+    }
+}
+
+fn what_path_can_we_talk_about(a: &Agent, b: &Agent) -> Topic {
+    let mut subject_weights = HashMap::new();
+    for aa in &[a, b] {
+        for (id_a, connections) in aa.mind.location_edges.iter() {
+            for id_b in connections {
+                let opinion = aa.mind.opinions_on_places[*id_a].abs().max(aa.mind.opinions_on_places[*id_b].abs());
+                subject_weights.insert((*id_a, *id_b), subject_weights.get(&(*id_a, *id_b)).unwrap_or(&0.0) + opinion);
+            }
+        }
+    }
+    let total_weight:f64 = subject_weights.values().sum();
+    if total_weight > 0.0 {
+        let mut rng = rand::thread_rng();
+        let keys:Vec<&(usize, usize)> = subject_weights.keys().collect();
+        match keys.choose_weighted(&mut rng, |k| subject_weights[k]) {
+            Ok((id_a, id_b)) => Topic::Path { id_a: *id_a, id_b: *id_b },
+            Err(_) => Topic::SmallTalk,
+        }
     } else {
         Topic::SmallTalk
     }
--- src/mind.rs
+++ src/mind.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use rand::Rng;
 use std::rc::Rc;
 use rand::prelude::SliceRandom;
-use super::conversation::simulate_conversation;
+use super::conversation::{simulate_conversation, Topic};
 
 use super::{Health, Location, Event, ActionSource, Agent};
 
@@ -12,6 +12,7 @@ pub struct Mind {
     pub opinions_on_others: Vec<f64>,
     pub preconceptions: Vec<f64>,
     pub opinions_on_places: Vec<f64>,
+    pub location_edges: HashMap<usize, Vec<usize>>,
     pub objects_seen: HashMap<usize, usize>,
 }
 
@@ -30,6 +31,7 @@ impl ActionSource  for Mind {
                 let loc = agent.location.clone();
                 let exits = loc.exits.borrow();
                 let new_location = exits.choose_weighted(&mut rng, |loc| *agent.mind.opinions_on_places.get(loc.id).  unwrap_or(&0.0) + 10000.0).unwrap().clone();
+                agent.mind.learn_edge(loc.id, new_location.id);
                 agent.events.push(Event { msg: format!("Moved from {} to {}", agent.location.id,
    new_location.id).to_string() });
                 agent.location = new_location;
@@ -55,11 +57,17 @@ impl ActionSource  for Mind {
                       Some(interlocular) => {
                           let conv = simulate_conversation(agent, &interlocular.borrow());
                           let mut interlocular = interlocular.borrow_mut();
+
                           agent.events.push(Event { msg: format!("Had {} with {} (initiated by me)", conv.to_string(), interlocular.name).to_string() });
                           interlocular.events.push(Event { msg: format!("Had {} with {} (initiated by them)", conv.to_string(), agent.name).to_string() });
+
                           agent.mind.cheer = (agent.mind.cheer + conv.tone * 0.01).max(-1.0).min(1.0);
-                          interlocular.mind.cheer = (interlocular.mind.cheer + conv.tone * 0.01)
-  .max(-1.0).min(1.0);
+                          interlocular.mind.cheer = (interlocular.mind.cheer + conv.tone * 0.01).max(-1.0).min(1.0);
+
+                          for topic in conv.topics {
+                              agent.mind.learn_about_topic(&topic, conv.tone);
+                              interlocular.mind.learn_about_topic(&topic, conv.tone);
+                          }
                       },
                       None => () // Nobody I care about around
                   };
@@ -79,6 +87,7 @@ impl Mind {
             opinions_on_others: Vec::with_capacity(1000),
             preconceptions: Vec::with_capacity(1000),
             opinions_on_places: Vec::with_capacity(1000),
+            location_edges: HashMap::with_capacity(1000),
             objects_seen: HashMap::with_capacity(1000),
         }
     }
@@ -99,14 +108,10 @@ impl Mind {
                 match a.try_borrow() {
                     Ok(a) => {
                         if a.health.alive {
-                            while self.opinions_on_others.len() <= a.id {
-                                self.preconceptions.push(rng.gen_range(-0.1, 0.1));
-                                self.opinions_on_others.push(0.0)
-                            }
-                            if self.opinions_on_others[a.id] == 0.0 {
+                            self.incr_opinion_on_person(a.id, d_opinion);
+                            if self.opinions_on_others[a.id] == d_opinion {
                                 view.events.push(Event { msg: format!("Met {}", a.name).to_string() });
                             }
-                            self.opinions_on_others[a.id] += d_opinion + self.preconceptions[a.id];
                         }
                     },
                     Err(_) => () // This is the current agent, fine.
@@ -117,12 +122,49 @@ impl Mind {
                 self.objects_seen.insert(o.id, view.location.id);
             }
 
-            
-            while self.opinions_on_places.len() <= view.location.id {
-                self.opinions_on_places.push(rng.gen_range(-0.001, 0.001));
-            }
-            self.opinions_on_places[view.location.id] += d_opinion;
+            self.incr_opinion_on_place(view.location.id, d_opinion);
+        }
+    }
+
+    fn incr_opinion_on_person(&mut self, id: usize, amount: f64) {
+        let mut rng = rand::thread_rng();
+        while self.opinions_on_others.len() <= id {
+            self.preconceptions.push(rng.gen_range(-0.1, 0.1));
+            self.opinions_on_others.push(0.0)
+        }
+        self.opinions_on_others[id] += amount + self.preconceptions[id];
+    }
+
+    fn incr_opinion_on_place(&mut self, id: usize, amount: f64) {
+        let mut rng = rand::thread_rng();
+        while self.opinions_on_places.len() <= id {
+            self.opinions_on_places.push(rng.gen_range(-0.001, 0.001));
+        }
+        self.opinions_on_places[id] += amount;
+    }
+
+    fn learn_edge(&mut self, id_a: usize, id_b: usize) {
+        match self.location_edges.get_mut(&id_a) {
+            Some(connections) => { connections.push(id_b); },
+            None => { self.location_edges.insert(id_a, vec![id_b]); },
+        };
+    }
+
+    fn learn_about_topic(&mut self, topic: &Topic, tone: f64) {
+        match topic {
+            Topic::Person{ id } => {
+                self.incr_opinion_on_person(*id, tone * 0.01);
+            },
+            Topic::Thing{ o_id, p_id } => {
+                self.objects_seen.insert(*o_id, *p_id);
+            },
+            Topic::Path{ id_a, id_b } => {
+                self.learn_edge(*id_a, *id_b);
+            },
+            Topic::Place{ id } => {
+                self.incr_opinion_on_place(*id, tone * 0.01);
+            },
+            _ => (),
         }
     }
 }
-
